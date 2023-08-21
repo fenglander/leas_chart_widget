b926ce42a89d4674a4276eab9d4d56aba0972315
135938504+fenglander@users.noreply.github.com <135938504+fenglander@users.noreply.github.com>
2023-08-21 15:08:26 +0800

    指定封面

--- leas_chart_widget/__manifest__.py
+++ leas_chart_widget/__manifest__.py
@@ -11,7 +11,7 @@
 
     'author': "Leas",
     'website': "http://www.leas.life",
-
+    'images': ['static/description/widget_preview.gif'],
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
